a9befc02d3196a7f2d8ec23b372d1256c5aba2ac
Sorethea OK <sorethea@gmail.com>
2022-12-12 15:19:26 +0700

    dev sorethea/hieat

--- src/AuthServiceProvider.php
+++ src/AuthServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvid
 use Sorethea\Hieat\Filament\Resources\Settings\RoleResource;
 use Sorethea\Hieat\Policies\Settings\ActivityPolicy;
 use Sorethea\Hieat\Policies\Settings\PermissionPolicy;
+use Sorethea\Hieat\Policies\Settings\RolePolicy;
 use Sorethea\Hieat\Policies\Settings\UserPolicy;
 use Spatie\Activitylog\Models\Activity;
 use Spatie\Permission\Models\Permission;
@@ -16,7 +17,7 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         User::class =>UserPolicy::class,
         Permission::class => PermissionPolicy::class,
-        Role::class => RoleResource::class,
+        Role::class => RolePolicy::class,
         Activity::class => ActivityPolicy::class,
     ];
     public function register()
