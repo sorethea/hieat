1d9e624488ee3561fff631bf1a62e5540af24e3c
Sorethea OK <sorethea@gmail.com>
2022-12-12 10:57:02 +0700

    dev sorethea/hieat

--- src/HieatServiceProvider.php
+++ src/HieatServiceProvider.php
@@ -3,10 +3,18 @@
 namespace Sorethea\Hieat;
 
 use Filament\PluginServiceProvider;
+use Sorethea\Hieat\Filament\Resources\Settings\PermissionResource;
+use Sorethea\Hieat\Filament\Resources\Settings\RoleResource;
+use Sorethea\Hieat\Filament\Resources\Settings\UserResource;
 use Spatie\LaravelPackageTools\Package;
 
 class HieatServiceProvider extends PluginServiceProvider
 {
+    protected array $resources = [
+        UserResource::class,
+        PermissionResource::class,
+        RoleResource::class,
+    ];
     public function packageConfigured(Package $package): void
     {
         $package->name("hieat")
