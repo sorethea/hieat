ea42191ab551d659bc03b21d475a63cf8cff07ef
Sorethea OK <sorethea@gmail.com>
2022-12-13 09:49:18 +0700

    dev sorethea/hieat

---
+++ src/Filament/Resources/Operation/CuisineResource/RelationManagers/RestaurantsRelationManager.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Sorethea\Hieat\Filament\Resources\Operation\CuisineResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Resources\Table;
+use Filament\Tables;
+
+class RestaurantsRelationManager extends RelationManager
+{
+    protected static string $relationship = 'restaurants';
+
+    protected static ?string $recordTitleAttribute = 'name';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }    
+}
