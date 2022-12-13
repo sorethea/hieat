4366c0feb2eab1b30397e9b1db3e3d51897ec4c8
Sorethea OK <sorethea@gmail.com>
2022-12-13 10:43:30 +0700

    dev sorethea/hieat

--- src/Filament/Resources/Settings/UserResource.php
+++ src/Filament/Resources/Settings/UserResource.php
@@ -47,7 +47,7 @@ class UserResource extends Resource
                         ->visibleOn("create")
                         ->required(),
                     Forms\Components\SpatieMediaLibraryFileUpload::make("avatar")
-                        ->collection('avatar'),
+                        ->collection('avatar')->image(),
                 ])->columnSpan(2),
                 Forms\Components\Card::make()->schema([
                     Forms\Components\BelongsToManyCheckboxList::make("roles")
