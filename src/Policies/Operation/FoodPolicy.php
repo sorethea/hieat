7b538cbcb208ef4f24f39a475fb565e0dd03d25b
Sorethea OK <sorethea@gmail.com>
2022-12-13 08:51:13 +0700

    dev sorethea/hieat

--- src/Policies/Operation/FoodPolicy.php
+++ src/Policies/Operation/FoodPolicy.php
@@ -12,30 +12,36 @@ class FoodPolicy
 
     public function viewAny(User $user): bool
     {
-
+        return $user->can("foods.index");
     }
 
     public function view(User $user, Food $food): bool
     {
+        return $user->can("foods.show");
     }
 
     public function create(User $user): bool
     {
+        return $user->can("foods.create");
     }
 
     public function update(User $user, Food $food): bool
     {
+        return $user->can("foods.edit");
     }
 
     public function delete(User $user, Food $food): bool
     {
+        return $user->can("foods.delete");
     }
 
     public function restore(User $user, Food $food): bool
     {
+        return $user->can("foods.restore");
     }
 
     public function forceDelete(User $user, Food $food): bool
     {
+        return $user->can("foods.destroy");
     }
 }
